45572cb5202566af79b619984f519fd80eb2cb05
Nicolas Lapointe <nixcocpp@gmail.com>
2018-09-26 14:45:05 -0400

    Ajout de la commande add

--- bot.py
+++ bot.py
@@ -1,39 +1,57 @@
 # use config.ENVVARNAME to use a env var
 import config
-import discord
 import random
+
+from discord.ext import commands
+
 
 # Go to `https://discordapp.com/oauth2/authorize?client_id=494271504570122240&scope=bot` to add this bot to a server
 
-client = discord.Client()
+description = '''
+Bot pour le channel discrod du club info
+'''
+bot = commands.Bot(command_prefix='!', description=description)
 
-@client.event
+@bot.command()
+async def add(left: int, right: int):
+    """Adds 2 numbers together"""
+    if left == 9 and right == 10:
+        await bot.say('{0} or something'.format(21))
+    else:
+        await bot.say(left + right)
+
+
+@bot.event
 async def on_message(msg):
-    if msg.author == client.user:
+    if msg.author == bot.user:
         return
 
+    # Process registered Commands
+    await bot.process_commands(msg)
+
     if msg.content == 'haha':
-        await client.send_message(msg.channel, 'yes')
+        await bot.send_message(msg.channel, 'yes')
 
     if msg.content.lower().find('nice') != -1:
-        await client.send_message(msg.channel, 'Fucking ***NICE***')
+        await bot.send_message(msg.channel, 'Fucking ***NICE***')
 
     if msg.content.lower().find('number 1') != -1:
-        await client.send_message(msg.channel, 'F Robbie Rotten, Born a villain, Died a Hero 😭')
+        await bot.send_message(msg.channel, 'F Robbie Rotten, Born a villain, Died a Hero 😭')
 
     if msg.content.lower().find('oof') != -1:
         choices = ['Big *OOF*', 'Roblox death sound', 'Oof oww my bones hurt a lot\nOof ouch oww owie my bonessss']
-        await client.send_message(msg.channel, random.choice(choices))
+        await bot.send_message(msg.channel, random.choice(choices))
 
     if msg.content == '!shutdown!' and msg.author.nick == 'Camarade Nicolas L.':
         exit()
 
 
-@client.event
+
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('-----')
 
-client.run(config.DISCORD_BOT_TOKEN)
+bot.run(config.DISCORD_BOT_TOKEN)
