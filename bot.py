0ceedbafca1117bb18efe1df7775d0cdc7616d6b
Nicolas Lapointe <nixcocpp@gmail.com>
2018-09-26 00:59:49 -0400

    Ajout de texte à réponse automatiques (Oof, haha, nice)

--- bot.py
+++ bot.py
@@ -1,24 +1,32 @@
 # use config.ENVVARNAME to use a env var
 import config
 import discord
+import random
 
 # Go to `https://discordapp.com/oauth2/authorize?client_id=494271504570122240&scope=bot` to add this bot to a server
 
 client = discord.Client()
 
 @client.event
-async def on_message(msg : discord.Message):
+async def on_message(msg):
     if msg.author == client.user:
         return
-
-    if(msg.author.nick == 'Étienne B.'):
-        await client.send_message(msg.channel, '**STOP**')
 
     if msg.content == 'haha':
         await client.send_message(msg.channel, 'yes')
 
     if msg.content.lower().find('nice') != -1:
         await client.send_message(msg.channel, 'Fucking ***NICE***')
+
+    if msg.content.lower().find('number 1') != -1:
+        await client.send_message(msg.channel, 'F Robbie Rotten, Born a villain, Died a Hero 😭')
+
+    if msg.content.lower().find('oof') != -1:
+        choices = ['Big *OOF*', 'Roblox death sound', 'Oof oww my bones hurt a lot\nOof ouch oww owie my bonessss']
+        await client.send_message(msg.channel, random.choice(choices))
+
+    if msg.content == '!shutdown!' and msg.author.nick == 'Camarade Nicolas L.':
+        exit()
 
 
 @client.event
